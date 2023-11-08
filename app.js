734a809e0b2b3ff4894ec0463f4459d6b09356b2
Tatiana_K <ottertut@gmail.com>
2023-11-08 14:38:17 +0200

    second

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ app.get('/', (req, res) => {
 	res.send("It works baby!")
 })
 
-app.listen(3000, () =>{
+const port = process.env.port || 3000
+
+app.listen(port, () => {
 	console.log('listening')
-})
+}
+)
 
