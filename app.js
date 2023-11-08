d9ac7c4b070a8703fb66c3291fa689a6b1bd0010
Tatiana_K <ottertut@gmail.com>
2023-11-08 19:06:37 +0200

    port

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 8080;
+/*global html*/
 
-app.get('/', (req, res) => {
-	res.send("It works baby!")
-})
+// New code
+app.get('/test', function (req, res) {
+    res.send('the REST endpoint test run!');
+});
 
-const port = process.env.port || 3000
 
-app.listen(port, () => {
-	console.log('listening')
-}
-)
-
+app.listen(port, function() {
+  console.log('Server running at http://127.0.0.1:%s', port);
+});
